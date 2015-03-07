067c80c7d060a2a81c6c2e26454f264c345b9106
Marcin Drozd <marcindrozd@me.com>
2015-03-07 19:09:11 +0100

    limit displayed recipes on main page in each category to 5 recent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
+
+  def recent_recipes
+    recipes.limit(5).order(created_at: :desc)
+  end
 end
