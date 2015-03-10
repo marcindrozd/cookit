19354f77df62a97fd3a93b2471520adfe0890d16
Marcin Drozd <marcindrozd@me.com>
2015-03-10 18:16:05 +0100

    add success message after registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      flash[:success] = "You've been successfully registered! You may log in now."
       redirect_to home_path
     else
       render :new
